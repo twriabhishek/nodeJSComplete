9904dd13f26bbddcb989d34d9f34eb05bf1c2d5b
Abhishek <abhishek130199@gmail.com>
2024-04-23 00:44:14 +0530

    Update blog portion and footer part completed

--- 22_utubeBlog/controllers/blog.controller.js
+++ 22_utubeBlog/controllers/blog.controller.js
@@ -29,6 +29,48 @@ const handleAddBlog = async (req, res) => {
 };
 
 
+
+const handleUpdateBlog = async (req, res) => {
+  const { title, body, coverImageUrl } = req.body;
+  const blogId = req.params.id;
+  const createdUser = req.user._id;
+
+  // Check if any required field is missing or blank
+  if (!title || title.trim() === "") {
+    return res.status(404).json({ error: "Title required" });
+  }
+
+  try {
+    
+    // Find the blog by ID
+    const blog = await Blog.findById(blogId);
+    // Check if the blog exists
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+    // Check if the current user is the creator of the blog
+    if (blog.createdBy.toString() !== createdUser) {
+      return res.status(403).json({ error: "You are not authorized to update this blog" });
+    }
+    // Update the blog fields
+    blog.title = title;
+    blog.body = body;
+    if (req.file.filename) {
+      blog.coverImageUrl = `/uploads/${req.file.filename}`;
+    }
+    // Save the updated blog
+    await blog.save();
+
+    // Send a success response
+    return res.redirect("/api/v1/blog/myBlog");
+  } catch (error) {
+    // Handle any errors
+    console.error("Error occurred during signup:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
 const handleAddComment = async(req, res) => {
   const { content } = req.body;
 
@@ -54,4 +96,4 @@ const handleAddComment = async(req, res) => {
   }
 };
 
-module.exports = { handleAddBlog, handleAddComment };
+module.exports = { handleAddBlog, handleAddComment, handleUpdateBlog };
--- 22_utubeBlog/route/blog.route.js
+++ 22_utubeBlog/route/blog.route.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-const { handleAddBlog, handleAddComment } = require("../controllers/blog.controller");
+const { handleAddBlog, handleAddComment,handleUpdateBlog } = require("../controllers/blog.controller");
 const Blog = require("../models/blog.model");
 const Comment = require("../models/comments.model");
+const { getUser } = require("../utils/auth.utils");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -23,19 +24,43 @@ router.get("/addBlog", (req, res) => {
   });
 });
 
+
+router.get("/myBlog", async(req, res) => {
+    // console.log(req.cookies.token);
+    const loginUser = getUser(req.cookies.token);
+    // console.log(loginUser);
+    const allBlogs = await Blog.find({createdBy:loginUser._id}).populate('createdBy');
+    return res.render("myBlogs.ejs", {
+      user: req.user,
+      blogs: allBlogs,
+    });
+});
+
+
+
+router.get("/update/:id", async(req, res) => {
+    const blog = await Blog.findById(req.params.id).populate('createdBy');
+    const comments = await Comment.find({blogId:req.params.id}).populate('createdBy');
+    return res.render("updateBlog.ejs", {
+      singleBlog: blog,
+      user: req.user,
+      comments
+    });
+});
+
 router.get("/:id", async(req, res) => {
     const blog = await Blog.findById(req.params.id).populate('createdBy');
     const comments = await Comment.find({blogId:req.params.id}).populate('createdBy');
-    console.log(comments);
     return res.render("singleBlogs.ejs", {
       singleBlog: blog,
       user: req.user,
       comments
     });
-  });
+});
 
 
-router.post("/addBlog", upload.single('coverImageUrl'), handleAddBlog);  
+router.post("/addBlog", upload.single('coverImageUrl'), handleAddBlog);
+router.post("/updateBlog/:id", upload.single('coverImageUrl'), handleUpdateBlog);  
 router.post("/addComment/:blogId", handleAddComment);
 
 
